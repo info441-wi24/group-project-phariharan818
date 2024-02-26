f5436d4c9c5e87a5c4edfb114978413b4e685a9d
kvajjhula <kriti.vajjhula@gmail.com>
2024-02-26 13:09:11 -0800

    new development setup

--- server/app.js
+++ server/app.js
@@ -4,14 +4,24 @@ import logger from 'morgan';
 import models from './models.js'
 import cors from "cors";
 import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
 dotenv.config()
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 const app = express();
+
 app.use(logger('dev'));
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'client/build')));
+
 
 // middleware to share model with api handlers
 app.use((req, res, next) => {
@@ -20,11 +30,13 @@ app.use((req, res, next) => {
 })
 
 app.get("/", (req, res) => {
-    res.status(200).send("Server is up and running!");
+    res.status(200).send("Server is up and running! new words more new words blah blah bloah");
 });
 
+
+
 app.get("/api", (req, res) => {
-    res.json({ "status": "success" })
+    res.json({ "status": "success", "yay": "hehe", "newvalue": "added", "newest value": "added" })
 })
 
 export default app;
