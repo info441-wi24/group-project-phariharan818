c7e905ccc29ce0a3fad7d663cd6c2ecd8371bd85
kvajjhula <kriti.vajjhula@gmail.com>
2024-02-26 13:08:41 -0800

    new development setup

---
+++ server/client/src/App.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+
+function App() {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        console.log(`${process.env.REACT_APP_API_URL}`)
+        const response = await fetch(`${process.env.REACT_APP_API_URL}/api`);
+        const data = await response.json();
+        console.log(data); 
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData(); // Call the function to fetch data
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload. this is my new word
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
