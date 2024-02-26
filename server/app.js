f8554615104c51e1cbd2c293d4d7958e4dbe110b
kvajjhula <kriti.vajjhula@gmail.com>
2024-02-25 20:37:22 -0800

    set up cra and simple express server with custom config

---
+++ server/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import models from './models.js'
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+app.use(logger('dev'));
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// middleware to share model with api handlers
+app.use((req, res, next) => {
+    req.models = models;
+    next()
+})
+
+app.get("/", (req, res) => {
+    res.status(200).send("Server is up and running!");
+});
+
+export default app;
