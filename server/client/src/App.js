d878b82beeee535beacb170739090bd987ebd93d
phariha <phariha@uw.edu>
2024-03-05 13:37:17 -0800

    nav bar added

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -1,14 +1,17 @@
 // import React, { useEffect } from 'react';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom'
-import HomePage from './components/HomePage';
+import JobPage from './components/JobPage';
+import Nav from './components/Nav.js'
 
 function App() {
   return (
     <div>
+      <Nav />
+      <div style={{ marginTop: '1em', padding: '10px' }}></div>
       <Routes>
         <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/" element={<HomePage />}  /> 
+        <Route path="/" element={<JobPage />}  /> 
       </Routes>
     </div>
   );
